a5d1d5901e43eb34283b9d63c386fd996e1ccb86
gpadd <unknown.areth@gmail.com>
2013-04-13 19:36:31 +0200

    Removing credits for myself from files I probably haven't touched..

--- irc/channel.scm
+++ irc/channel.scm
@@ -1,6 +1,4 @@
 ;; Copyright (C) 2012 bas smit (fbs)
-;; Copyright (C) 2013 Andreas W (add^_)
-
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Lesser General Public License
